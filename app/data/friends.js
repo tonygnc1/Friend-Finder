878c85377f8af093cdb3797dd3d0b5d08f6bf70e
tonygnc1 <bootcamp1955@gmail.com>
2019-08-30 09:57:30 -0400

    Heroku commit

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+        name: "John Doe",
+        photo: <img src="./Beevis.jpg" />,
+        scores: [5, 3, 5, 3, 5, 3, 5, 3, 5, 3]
+    },
+
+    {
+        name: "Jim Doe",
+        photo: <img src="./Butthead" />,
+        scores: [2, 3, 4, 3, 6, 3, 6, 3, 5, 3]
+    },
+
+    {
+        name: "John Doe",
+        photo: <img src="./homer.jpg" />,
+        scores: [3, 3, 4, 3, 5, 2, 5, 7, 5, 3]
+    },
+];
+
+module.exports= friends;
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
     app.get("/", function(req, res) {
-        console.log(
-            "gthyju"
-        )
+        // console.log(
+            // "gthyju"
+        // )
         res.sendFile(path.join(__dirname, "./public/home.html"))
     });
     app.get("/survey", function(req, res) {
         console.log(
             "gthyju"
         )
-        res.sendFile(path.join(__dirname, "./public/survey.html"))
+        res.sendFile(path.join(__dirname, "/survey.html"))
     });
 app.listen(PORT, function(){
     console.log(`listening on ${PORT}`)
